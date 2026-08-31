e094cc2c5085e49e0c6c90552e93d72340af0f7d
Yann <ad@p-difm.com>
2026-08-31 15:55:46 +0200

    feat: Configuration PWA (manifest, service worker, icons)

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+﻿const CACHE_NAME = "drillflow-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/js/i18n.js",
+  "/js/data/vocabulary.js",
+  "/manifest.json",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache hit or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
